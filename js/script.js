c15bf2e928a68f4ded83ad92c695381796031a00
Alan Hernandez <alan.m.hernandez@gmail.com>
2017-04-21 15:54:09 -0400

    Established Basic Frame of Site with Most Info

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 $("document").ready(function() {
+
+  // Header fade in on page load
+  $(".fade-in").fadeIn(3000);
+
+  // Fade in/out when scrolling to/from header
+  $(window).scroll(function(){
+    $("#home>.container").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
+
+  // Footer Same Height as Navbar
   var height = $(".navbar-default").height();
   $("footer").css("height", height);
+
+  $(".navbar-fixed-top").autoHidingNavbar();
+
+  // Navbar Invisible at top of Page
+  // $('.navbar').css('visibility','hidden');
+
+  // Visible Navbar after scroll
+  // $(window).scroll(function () {
+  //   if ($(window).scrollTop() = '#') {
+  //     $(".navbar").css('visibility', 'visibile');
+  //   }
+  //   else if ($(window).scrollTop() = 0) {
+  //     $('.navbar').css('visibility','hidden');
+  //   }
+  // });
 
   // Highlight the top nav as scrolling occurs
   $('body').scrollspy({target: '.navbar-fixed-top'});
