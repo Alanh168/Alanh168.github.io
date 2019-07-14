dea19b469cf4041474ea6922cb1b28ce6c63dd3b
Alanh168 <alan.m.hernandez@gmail.com>
2019-07-14 12:02:33 -0400

    Added Loader

--- js/script.js
+++ js/script.js
@@ -8,6 +8,14 @@ $("document").ready(function() {
        }
      });
 
+// Loader
+  // Show loader until page load
+
+  // Display Page on Load when 2 seconds have passed
+  setTimeout(function(){
+    $(".loader-container").css({"display": "none"});
+  }, 2000);
+
 // All Navbar-related Scripts
   // Animate buttons on click
   var buttons = $(".nav li a, .nav-tabs li a, .nav-stacked li a");
@@ -28,6 +36,6 @@ $("document").ready(function() {
   });
   project.mouseleave(function() {
     console.log("Testing mouseleave");
-    $(".project p").css("display", "none");    
+    $(".project p").css("display", "none");
   });
 });
